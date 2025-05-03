2ca97c3f8441d5a329cf497f829c9d81f7dd6ec3
thomasmfx <thomasmoisesf@gmail.com>
2025-05-02 21:46:04 -0300

    🌟 feat: Configure app

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes/index.js');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', routes.index);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`),
+);
