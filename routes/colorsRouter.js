3aa416cb61f923dda6736b336dfbed3224dcdb28
thomasmfx <thomasmoisesf@gmail.com>
2025-05-04 16:17:31 -0300

    🌟 feat: Add color details page
    - Adds the route, controller, template, styles, and script necessary to display the details page for a specific color

--- routes/colorsRouter.js
+++ routes/colorsRouter.js
@@ -4,6 +4,10 @@ const colorsController = require('../controllers/colorsController');
 
 const colorsRouter = Router();
 
-colorsRouter.get('/', colorsController.getColorsList);
+colorsRouter.get('/:id', colorsController.getColor);
+colorsRouter.get('/', colorsController.getAllColors);
+colorsRouter.post('/', colorsController.createColor);
+colorsRouter.put('/:id', colorsController.updateColor);
+colorsRouter.delete('/:id', colorsController.deleteColor);
 
 module.exports = colorsRouter;
